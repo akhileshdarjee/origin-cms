4f798a7e2d502a52002ec0c37541f7489f477cf5
Akhilesh Darjee <akhileshdarjee@gmail.com>
2016-02-22 21:55:10 +0530

    removed users other than admin from seeders

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,8 +12,7 @@ class UserSeeder extends Seeder
 	public function run()
 	{
 		$users = array(
-			['full_name' => 'Administrator', 'avatar' => null, 'login_id' => 'admin', 'password' => bcrypt('admin@111'), 'email' => 'akhileshdarjee@gmail.com', 'role' => 'Administrator', 'owner' => 'admin', 'last_updated_by' => 'admin', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-			['full_name' => 'Administrator', 'avatar' => null, 'login_id' => 'ankit', 'password' => bcrypt('ankit@111'), 'email' => 'ankit@gmail.com', 'role' => 'Guest', 'owner' => 'admin', 'last_updated_by' => 'admin', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')]
+			['full_name' => 'Administrator', 'avatar' => null, 'login_id' => 'admin', 'password' => bcrypt('admin@111'), 'email' => 'akhileshdarjee@gmail.com', 'role' => 'Administrator', 'owner' => 'admin', 'last_updated_by' => 'admin', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')]
 		);
 
 		DB::table('tabUser')->insert($users);
